ca5dc52ac5d0dff072f214f21c0f166a13646a76
Woomymy <woomy@woomy.be>
2024-10-30 13:56:06 +0100

    rock: Configure wifi stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/tree/master/vendor/wlan
    
    Co-Authored-By: bengris32 <bengris32@protonmail.ch>
    Co-Authored-By: electimon <electimon@gmail.com>
    
    Change-Id: I56eb9df48b866173ad0ec706d6929867c1b3a342
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -122,6 +122,17 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc) \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/seccomp/,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy)
 
+# Wifi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi-service \
+    wpa_supplicant \
+    hostapd \
+    libwifi-hal-wrapper:64
+
+# Wifi configs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
+
 # Permissions (features)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
@@ -166,8 +177,6 @@ PRODUCT_PACKAGES += \
     init.insmod.sh \
     init.insmod.mtk.cfg \
     init.cgroup.rc \
-    init_connectivity.rc \
-    init.connectivity.common.rc \
     init.connectivity.rc \
     init.mt6789.rc \
     init.mt6789.usb.rc \
