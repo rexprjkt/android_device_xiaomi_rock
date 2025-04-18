8af069c6d98e2029d4c5468cb0c1db0e7d984c8f
xyzuniverse <dev.xyzuniverse@gmail.com>
2025-04-18 12:13:40 +0800

    rock: Update fingerprint description
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- lineage_rock.mk
+++ lineage_rock.mk
@@ -24,4 +24,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="missi_phone_global_ext4-user 14 UP1A.231005.007 V816.0.12.0.ULUMIXM release-keys" \
-    BuildFingerprint=Android/missi_phone_global_ext4/missi:14/UP1A.231005.007/V816.0.12.0.ULUMIXM:user/release-keys
+    BuildFingerprint=POCO/stone_p_global/stone:13/TP1A.220624.014/V816.0.12.0.ULUMIXM:user/release-keys
